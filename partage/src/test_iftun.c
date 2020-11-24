6fdf3be595ce6db175d5ea6d617262bb8c600bf9
William <william.barry@etu.univ-amu.fr>
2020-11-24 17:11:14 +0100

    déplacement des sources dans un répertoire correspondant

--- partage/src/test_iftun.c
+++ partage/src/test_iftun.c
@@ -1,6 +1,7 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 #include <sys/socket.h> 
 #include <sys/stat.h>
 #include <sys/ioctl.h>
@@ -17,7 +18,6 @@ int tun_alloc(char *dev)
 {
   struct ifreq ifr;
   int fd, err;
-
   if( (fd = open("/dev/net/tun", O_RDWR)) < 0 ){
     perror("alloc tun");
     exit(-1);
@@ -38,21 +38,51 @@ int tun_alloc(char *dev)
     close(fd);
     return err;
   }
+
   strcpy(dev, ifr.ifr_name);
+
   return fd;
 }      
 
+void catchPackets(char *tun_name, char *dest){
+	int tunfd;
+	unsigned char buffer[1500];
+	int nread = 0;
+	char command[50];
+
+	tunfd = tun_alloc(tun_name);
+
+	
+	strcpy(command, "bash configure-tun.sh");
+
+	system(command);
+	if(tunfd < 0){
+    	perror("Allocating interface");
+    	exit(1);
+  	}
+
+
+	while(1) {
+	    nread = read(tunfd,buffer,sizeof(buffer));
+	    if(nread < 0) {
+	    	perror("Reading from interface");
+	    	close(tunfd);
+	    	exit(1);
+	    }
+
+	    printf("Read %d bytes from device %s\n", nread, tun_name);
+ 	}
+}
+
 int main (int argc, char** argv){
 
-  int tunfd;
-  printf("Création de %s\n",argv[1]);
-  tunfd = tun_alloc(argv[1]);
-  printf("Faire la configuration de %s...\n",argv[1]);
-  printf("Appuyez sur une touche pour continuer\n");
-  printf("Interface %s Configurée:\n",argv[1]);
-  system("ip addr");
-  printf("Appuyez sur une touche pour terminer\n");
-  getchar();
+	if (argc != 3) {
+		printf("Usage Error \n");
+		return 1;
+  	}
 
-  return tunfd;
+  	catchPackets(argv[1], argv[2]);
+	
+	return 0;
 }
+
